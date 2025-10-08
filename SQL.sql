64081239e7a1a69b23c7dfb22778d582b517c819
Sarabpreet1023 <161135692+Sarabpreet1023@users.noreply.github.com>
2025-10-08 14:01:19 +0530

    Create SQL.sql

---
+++ SQL.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE your_database_name;
+USE your_database_name;
+
+CREATE TABLE Student (
+    StudentID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(100),
+    Department VARCHAR(50),
+    Marks DOUBLE
+);
